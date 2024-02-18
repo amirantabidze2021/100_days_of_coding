57c4c41f015b82945347446b8bb023e3c1baff35
Amiran Tabidze <81627817+amirantabidze2021@users.noreply.github.com>
2024-02-18 11:01:43 +0400

    Update day_1_Debugging exercises.py

--- day_1/day_1_Debugging exercises.py
+++ day_1/day_1_Debugging exercises.py
@@ -1,6 +1,5 @@
-"""
 print(Day-1 -String manipulation ")
 print("String concantenation is done with the " + " sign.")
     print('e.g. print("Hello world!)')
-print(("New lne can create backslash and n "))
-"""
+print(("New lne can create backslash and n ")) # print function
+
